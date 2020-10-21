d63d624fa181201226a1daa8b8c00097f0fb521a
QuangNam <quangnamhd9x@gmail.com>
2020-10-21 11:27:28 +0700

    first commit

---
+++ demo.js
@@ -0,0 +1,9 @@
+function Hello(lop) {
+    console.log("Chao Nam" + lop);
+}
+Hello("C06");
+function Goodbye() {
+    return "Tam biet Nam";
+}
+var x = Goodbye();
+console.log(x);
---
+++ demo.ts
@@ -0,0 +1,12 @@
+function Hello(lop:string){     // voi ham nhu nay chi can goi ham
+    console.log("Chao Nam" + lop);
+}
+
+Hello("C06");
+
+function Goodbye(){     // voi ham nay phai co bien hung no de hien thi
+    return "Tam biet Nam";
+}
+
+var x:string = Goodbye();
+console.log(x);
